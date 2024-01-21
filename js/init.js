47eaee391e5499cfa689ec4ded0d6f543dc9f453
raissalves <117692349+raissalves@users.noreply.github.com>
2024-01-21 17:56:41 -0300

    atualização carbonara

--- js/init.js
+++ js/init.js
@@ -1,7 +1,38 @@
-(function($){
-  $(function(){
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $(".dropdown-trigger").dropdown();
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
 
-    $('.sidenav').sidenav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    toolbarEnabled: true
+  });
+});
+
+// Or with jQuery
+
+$('.fixed-action-btn').floatingActionButton({
+  toolbarEnabled: true
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
